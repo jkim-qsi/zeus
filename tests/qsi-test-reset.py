0dd44d894528e7ff54298299e9573dc31e74e183
Jaehwan Kim <jkim@quantum-si.com>
2024-06-12 14:54:59 -0400

    code for dispense at bottom tests

---
+++ tests/qsi-test-reset.py
@@ -0,0 +1,35 @@
+#%%
+import zeus
+# import ezstepper
+import os
+from time import sleep
+
+# os.add_dll_directory(R"C:\Users\Lab\Downloads\usb2can_canal_v2.0.0\usb2can_canal_v2.0.0\x64\Release")
+
+zm = zeus.ZeusModule(1, init_module=False) # 1 is set by the jumper on the Zeus X1 module
+
+deck = zeus.DeckGeometry(index=0, endTraversePosition=600, beginningofTipPickingPosition=1100, positionofTipDepositProcess=1200)
+water_container = zeus.ContainerGeometry(index=0, diameter=200, bottomSection=10000, bottomPosition=1800, immersionDepth=20)
+
+# Liquid class 23 for 300uL clear tip with filter
+lc_water_300uL = zeus.LiquidClass(index=23, liquidClassForFilterTips=1, \
+                                  aspirationMode=0, aspirationFlowRate=500, aspirationSwapSpeed=1000, aspirationSettlingTime=10, \
+                                  lld=1, plldSensitivity=3, \
+                                  dispensingMode=0, dispensingFlowRate=500, stopFlowRate=500, dispensingSettlingTime=10 \
+                                  )
+cc_water_300uL_aspirate = zeus.calibrationCurve(index=lc_water_300uL.index, direction='aspirate')
+cc_water_300uL_dispense = zeus.calibrationCurve(index=lc_water_300uL.index, direction='dispense')
+
+# reset
+# zm.moveZDrive(1800, speed=900)
+# zm.moveZDrive(1980, speed=100)
+# sleep(2.0)
+# zm.moveZDrive(1980, speed=100)
+
+zm.moveZDrive(600, speed=900)
+
+
+# sleep(5.0)
+# input('Press any key to dispense')
+# zm.simpleDispense(volume=300)
+# sleep(1.0)
--- tests/qsi-test-setup.py
+++ tests/qsi-test-setup.py
@@ -6,7 +6,7 @@ from time import sleep
 
 # os.add_dll_directory(R"C:\Users\Lab\Downloads\usb2can_canal_v2.0.0\usb2can_canal_v2.0.0\x64\Release")
 
-zm = zeus.ZeusModule(1, init_module=False) # 1 is set by the jumper on the Zeus X1 module
+zm = zeus.ZeusModule(1, init_module=True) # 1 is set by the jumper on the Zeus X1 module
 
 deck = zeus.DeckGeometry(index=0, endTraversePosition=600, beginningofTipPickingPosition=1100, positionofTipDepositProcess=1200)
 water_container = zeus.ContainerGeometry(index=0, diameter=200, bottomSection=10000, bottomPosition=1800, immersionDepth=20)
@@ -20,56 +20,27 @@ lc_water_300uL = zeus.LiquidClass(index=23, liquidClassForFilterTips=1, \
 cc_water_300uL_aspirate = zeus.calibrationCurve(index=lc_water_300uL.index, direction='aspirate')
 cc_water_300uL_dispense = zeus.calibrationCurve(index=lc_water_300uL.index, direction='dispense')
 
-# # Set parameters
-# zm.setDeckGeometryParameters(deck)
-# sleep(0.5)
-# zm.setContainerGeometryParameters(water_container)
-# sleep(0.5)
-# zm.setLiquidClassParameters(lc_water_300uL)
-# sleep(0.5)
-# zm.setCalibrationCurveParameters(cc_water_300uL_aspirate)
-# sleep(0.5)
-# zm.setCalibrationCurveParameters(cc_water_300uL_dispense)
-# sleep(0.5)
+# Set parameters
+zm.setDeckGeometryParameters(deck)
+sleep(0.5)
+zm.setContainerGeometryParameters(water_container)
+sleep(0.5)
+zm.setLiquidClassParameters(lc_water_300uL)
+sleep(0.5)
+zm.setCalibrationCurveParameters(cc_water_300uL_aspirate)
+sleep(0.5)
+zm.setCalibrationCurveParameters(cc_water_300uL_dispense)
+sleep(0.5)
 
-# input("Place tips under pipette head and press any key to continue")
-# # zm.pickUpTip(4, 0) # tip type 4: 300uL conductive non-filtered
-# zm.pickUpTip(5, 0) # tip type 5: 300uL conductive filtered
-
-
-input("Place sample container under pipette head and press any key to continue")
-zm.moveZDrive(1300, speed=900)
-sleep(2.0)
-zm.simpleAspirate(volume=300)
-sleep(2.0)
-zm.moveZDrive(600, speed=800) #preset_speed='slow')
+input("Place tips under pipette head and press any key to continue")
+# zm.pickUpTip(4, 0) # tip type 4: 300uL conductive non-filtered
+zm.pickUpTip(5, 0) # tip type 5: 300uL conductive filtered
 
 
-input("Remove sample container under pipette head and position chip")
-# zm.bottomSearch(start=1900, stop=2500)
-zm.moveZDrive(1800, speed=900)
-sleep(3.0)
-zm.moveZDrive(1985, speed=100)
-sleep(3.0)
-
-for idx in range(9):
-    print('{} out of 9'.format(idx))
-    zm.simpleDispense(volume=290)
-    sleep(1.5)
-    zm.simpleAspirate(volume=290)
-    sleep(1.5)
-zm.simpleDispense(volume=300)
-sleep(1.5)
-
-# reset
-zm.moveZDrive(600, speed=900)
-sleep(10.0)
-# zm.simpleDispense(volume=300)
-
+# input("Place sample container under pipette head and press any key to continue")
 # zm.aspiration(aspirationVolume=300, deckGeometryTableIndex=0, liquidClassTableIndex=23, lld=1)
-# zm.aspirate_lld(aspirationVolume=300, deckGeometryTableIndex=0, liquidClassTableIndex=23)
-# zm.getErrorCode()
-# input("Position chip under pipette head and press any key to continue")
+
+# input("Posiion chip under pipette head and press any key to continue")
 
 # zm.initZDrive()
 
