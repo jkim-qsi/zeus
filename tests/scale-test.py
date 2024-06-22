3ac322a71025b14f9a07f293741d1d9fda7ce8ff
Jaehwan Kim <jkim@quantum-si.com>
2024-06-22 14:37:21 -0400

    Update stage positions

--- tests/scale-test.py
+++ tests/scale-test.py
@@ -13,12 +13,14 @@ sm.flush_input_buffer()
 
 
 # Initialize pipette head
-zm = zeus.ZeusModule(1, init_module=True, discard_tip=False) # 1 is set by the jumper on the Zeus X1 module
+zm = zeus.ZeusModule(1, init_module=True, discard_tip=True) # 1 is set by the jumper on the Zeus X1 module
 #%%
-deck = zeus.DeckGeometry(index=0, endTraversePosition=600, beginningofTipPickingPosition=1700, positionofTipDepositProcess=1700)
+deck = zeus.DeckGeometry(index=0, endTraversePosition=600, beginningofTipPickingPosition=1600, positionofTipDepositProcess=1600)
 zm.setDeckGeometryParameters(deck)
 #%%
 water_container = zeus.ContainerGeometry(index=0, diameter=200, bottomSection=10000, bottomPosition=1550, immersionDepth=20)
+zm.setContainerGeometryParameters(water_container)
+
 #%%
 # Liquid class 23 for 300uL clear tip with filter
 lc_water_300uL = zeus.LiquidClass(index=23, liquidClassForFilterTips=1, \
@@ -26,26 +28,46 @@ lc_water_300uL = zeus.LiquidClass(index=23, liquidClassForFilterTips=1, \
                                   lld=1, plldSensitivity=3, \
                                   dispensingMode=0, dispensingFlowRate=500, stopFlowRate=500, dispensingSettlingTime=10 \
                                   )
+zm.setLiquidClassParameters(lc_water_300uL)
+#%%
 cc_water_300uL_aspirate = zeus.calibrationCurve(index=lc_water_300uL.index, direction='aspirate')
+zm.setCalibrationCurveParameters(cc_water_300uL_aspirate)
+#%%
 cc_water_300uL_dispense = zeus.calibrationCurve(index=lc_water_300uL.index, direction='dispense')
+zm.setCalibrationCurveParameters(cc_water_300uL_dispense)
 
 
 #%%
 
-# zm.moveZDrive(1500, speed=400)
+zm.moveZDrive(1600, speed=400)
 
 #%%
-sm_z = 195
-sm_y = 193
-sm_x = {
-    'tip_pickup': 5,
-    'over_scale': 115,
-    }
+sm_pos = {
+    'park':       [0, 253, 175],
+    'tip_pickup': [-3.1,   246.5, 95],
+    'over_scale': [205, 193, 175],
+}
+# sm_z = {
+#     'tip_pickup': 175,
+#     'over_scale: 175
+# }
+# sm_y = {
+#     'tip_pickup': 193,
+#     'over_scale': 193
+# }
+# sm_x = {
+#     'tip_pickup': 5,
+#     'over_scale': 115,
+#     }
 #%%
 
 # Pipette tip tray 
 # input('Press any key to pickup tip')
-sm.move((sm_x['tip_pickup'], sm_y, sm_z))
+# sm.move((sm_x['tip_pickup'], sm_y, sm_z))
+sm.move(sm_pos['tip_pickup'])
+
+#%%
+# sm.move(sm_pos['park'])
 #%%
 
 # sleep(1.0)
@@ -54,20 +76,22 @@ zm.pickUpTip(5, 0) # tip type 5: 300uL conductive filtered)
 #%%
 zm_z = {
     'travel': 1000,
-    'surface': 2250,
-    'aspirate': 2255,
-    'dispense': 2250
+    'surface': 2025,
+    'aspirate': 2040,
+    # 'dispense': 2040 # on small plate
+    'dispense': 1700 # in water bottle
 }
 zm_speed = 400
 #%%
 # water container position
-sm.move((sm_x['over_scale'], sm_y, sm_z))
+# sm.move((sm_x['over_scale'], sm_y, sm_z))
+sm.move(sm_pos['over_scale'])
 
 #%%
 zm.moveZDrive(zm_z['aspirate'], speed=zm_speed)
 sleep(5.0)
 #%%
-zm.simpleAspirate(volume=1000)
+zm.simpleAspirate(volume=2000)
 # sleep(5.0)
 #%%
 zm.moveZDrive(zm_z['dispense'], speed=zm_speed)
@@ -76,7 +100,7 @@ zm.moveZDrive(zm_z['dispense'], speed=zm_speed)
 zm.moveZDrive(zm_z['travel'], speed=zm_speed)
 
 #%%
-volume = 50
+volume = 100
 # zm.moveZDrive(zm_z['dispense'], speed=zm_speed)
 
 zm.simpleDispense(volume=volume)
